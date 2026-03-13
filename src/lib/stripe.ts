f4b1a3eb954d24508a04a9ec8642472188f032be
Felix <felix@freelancerratecalc.com>
2026-03-13 16:46:06 -0400

    Fix Stripe API version for deployment

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,9 +4,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY is not set in environment variables')
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-12-18.acacia',
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export async function createCheckoutSession() {
   try {
